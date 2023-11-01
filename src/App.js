86c8cfe9490f2f14a3a073566ff546b3878bad26
Codebraker7 <snehaagg7304@gmail.com>
2023-11-01 23:05:52 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Services from './components/layout/Services';
